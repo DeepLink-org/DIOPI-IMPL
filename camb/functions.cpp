49299d06cff94ea648611a54e92fd9d3a33c832c
gfyts0123 <zhyanwentao@126.com>
2023-02-24 19:08:24 +0800

    feature: bn 4d forward infer

--- camb/functions.cpp
+++ camb/functions.cpp
@@ -21,8 +21,15 @@
                     ret, ::cnnlGetErrorString(ret), __FILE__, __LINE__);               \
         }}\
 
+#define DIOPI_CHECK(cond, str) \
+    if (!(cond)) { \
+        impl::camb::set_last_error_string("%s at %s:%d", str, __FILE__, __LINE__); \
+        return diopiErrorOccurred; \
+    } \
+
+
 template<typename T, ::cnnlStatus_t(*fnCreate)(T*), ::cnnlStatus_t(*fnDestroy)(T)>
-class CnnlResourceGuard final {
+class CnnlResourceGuard {
 public:
     CnnlResourceGuard() {
         DIOPI_CHECKCNNL(fnCreate(&resource_));
@@ -39,6 +46,95 @@ public:
 protected:
     T resource_ {0};
 };
+
+class CnnlTransposeDescriptor final
+    : public CnnlResourceGuard<cnnlTransposeDescriptor_t,
+          cnnlCreateTransposeDescriptor, cnnlDestroyTransposeDescriptor> {
+public:
+    CnnlTransposeDescriptor() {}
+
+    CnnlTransposeDescriptor(const int dim, const int* permute) {
+        set(dim, permute);
+    }
+
+    diopiError_t set(const int dim, const int* permute) {
+        DIOPI_CALLCNNL(cnnlSetTransposeDescriptor(get(), dim, permute));
+        return diopiSuccess;
+    }
+};
+
+class CnnlTensorDescriptor final : public CnnlResourceGuard<cnnlTensorDescriptor_t,
+        cnnlCreateTensorDescriptor, cnnlDestroyTensorDescriptor>
+{
+public:
+  CnnlTensorDescriptor() {}
+  CnnlTensorDescriptor(auto &t, cnnlTensorLayout_t layout) {
+    set(t, layout);
+  }
+  template<typename T>
+  diopiError_t set(T& t, cnnlTensorLayout_t layout) {
+    int dimNb = t.dim();
+    auto dimSize = t.shape().data;
+    cnnlDataType_t dtype;
+    DIOPI_CALL(convertType(&dtype, t.dtype()));
+
+    std::vector<int> shape_info(dimNb);
+    if (layout == CNNL_LAYOUT_NHWC || layout == CNNL_LAYOUT_NDHWC
+            || layout == CNNL_LAYOUT_NLC) {
+        shape_info[0] = dimSize[0];
+        for (size_t i = 0; i < dimNb - 1; ++i) {
+            shape_info[i+1] = dimSize[(i + 1) % (dimNb - 1) + 1];
+        }
+    } else if (layout == CNNL_LAYOUT_HWCN) {
+        // HWCN is only used by depthwise conv now, and the dim is 4
+        DIOPI_CHECK(dimNb == 4, "depthwise convolution input's dim must be 4!");
+        shape_info[0] = dimSize[2];
+        shape_info[1] = dimSize[3];
+        shape_info[2] = dimSize[1];
+        shape_info[3] = dimSize[0];
+    } else {
+        for (size_t i = 0; i < dimNb; ++i) {
+            shape_info[i] = dimSize[i];
+        }
+    }
+    DIOPI_CALLCNNL(cnnlSetTensorDescriptor(this->get(), layout,
+                                                dtype, dimNb, shape_info.data()));
+    return diopiSuccess;
+}
+};
+
+template<typename T>
+static diopiError_t cnnl_transpose(diopiContextHandle_t& ctx, cnnlHandle_t& handle, T& in, impl::camb::DiopiTensor<diopiTensorHandle_t>& out, cnnlTensorLayout_t layoutIn,
+                          cnnlTensorLayout_t layoutOut) {
+    std::vector<int> order;
+    if (layoutIn == CNNL_LAYOUT_NHWC && layoutOut == CNNL_LAYOUT_HWCN) {
+        order = {1, 2, 3, 0};
+    } else if (layoutIn == CNNL_LAYOUT_NHWC && layoutOut == CNNL_LAYOUT_NCHW) {
+        order = {0, 3, 1, 2};
+    } else if (layoutIn == CNNL_LAYOUT_NCHW && layoutOut == CNNL_LAYOUT_HWCN) {
+        order = {2, 3, 1, 0};
+    } else if (layoutIn == CNNL_LAYOUT_NCHW && layoutOut == CNNL_LAYOUT_NHWC) {
+        order = {0, 2, 3, 1};
+    } else if (layoutIn == CNNL_LAYOUT_HWCN && layoutOut == CNNL_LAYOUT_NHWC) {
+        order = {3, 0, 1, 2};
+    } else if (layoutIn == CNNL_LAYOUT_HWCN && layoutOut == CNNL_LAYOUT_NCHW) {
+        order = {3, 2, 0, 1};
+    } else {
+        impl::camb::set_last_error_string("unkown layout error, layout should be in [CNNL_LAYOUT_NHWC, CNNL_LAYOUT_NCHW, CNNL_LAYOUT_HWCN], at %s:%s", __FILE__, __LINE__);
+        return diopiDtypeNotSupported;
+    }
+    CnnlTensorDescriptor inDesc(in, layoutIn);
+    CnnlTensorDescriptor outDesc(out, layoutOut);
+    CnnlTransposeDescriptor transDesc(order.size(), order.data());
+    size_t workspace_size = 0;
+    DIOPI_CHECKCNNL(cnnlGetTransposeWorkspaceSize(handle, inDesc.get(), transDesc.get(), &workspace_size));
+
+    void* workspace_ptr = workspace_size== 0 ? impl::camb::requiresBuffer(ctx, workspace_size).data() : nullptr;
+    DIOPI_CALLCNNL(cnnlTranspose_v2(handle, transDesc.get(), inDesc.get(),
+                                      in.data(), outDesc.get(), out.data(),
+                                      workspace_ptr, workspace_size));
+    return diopiSuccess;
+}
 
 static diopiError_t convertType(cnnlDataType_t *cnnlType, diopiDtype_t type) {
     switch (type) {
@@ -127,6 +223,206 @@ diopiError_t diopiFill(diopiContextHandle_t ctx, diopiTensorHandle_t input, cons
     DIOPI_CALLCNNL(cnnlSetTensorDescriptorEx(desc, layout, dtype, dimNb,
         dimSize.data(), dimStrides.data()));
     DIOPI_CALLCNNL(cnnlFill(handle, val, desc, trInput.data()));
+}
+
+
+diopiError_t diopiBatchNorm(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiTensorHandle_t save_mean,
+                                      diopiTensorHandle_t save_invstd, diopiConstTensorHandle_t input, diopiConstTensorHandle_t weight,
+                                      diopiConstTensorHandle_t bias, diopiTensorHandle_t running_mean,
+                                      diopiTensorHandle_t running_var, bool training, double momentum, double eps) {
+    /* Generate Tensors */
+    auto save_mean_tr = impl::camb::makeTensor(save_mean);
+    auto save_invstd_tr = impl::camb::makeTensor(save_invstd);
+    auto input_tr = impl::camb::makeTensor(input);
+    auto weight_tr = impl::camb::makeTensor(weight);
+    auto bias_tr = impl::camb::makeTensor(bias);
+    auto running_mean_tr = impl::camb::makeTensor(running_mean);
+    auto running_var_tr = impl::camb::makeTensor(running_var);
+    auto output_tr = impl::camb::makeTensor(out);
+
+    /* Some basic check */
+    DIOPI_CHECK(running_mean_tr.dtype() ==  running_var_tr.dtype(), "running_mean and running_var need to have the same data types");
+    // TODO: 2,3,5 dim support
+    DIOPI_CHECK(input_tr.dim() >= 4 && input_tr.dim() <=4, "Input dim is out of range");
+    DIOPI_CHECK(input_tr.dim() == output_tr.dim(), "Input dim != out dim");
+
+    /* Get current handle*/
+    auto stream = impl::camb::getStream(ctx);
+    CnnlResourceGuard<cnnlHandle_t, cnnlCreate, cnnlDestroy> CnnlHandle;
+    cnnlHandle_t handle = CnnlHandle.get();
+    DIOPI_CALLCNNL(cnnlSetQueue(handle, stream));
+
+    /* Transpose NCHW to NHWC */
+    auto memory_format = impl::camb::MemoryFormat::ChannelsLast;
+    auto input_channel_last = input_tr.contiguous(ctx, memory_format);
+    cnnl_transpose(ctx, handle, input_tr, input_channel_last, CNNL_LAYOUT_NCHW, CNNL_LAYOUT_NHWC);
+    auto output_channel_last = output_tr.contiguous(ctx, memory_format);
+    // cnnl_transpose(ctx, handle, output_tr, output_channel_last, CNNL_LAYOUT_NCHW, CNNL_LAYOUT_NHWC);
+
+    CnnlTensorDescriptor weight_bias_mean_var_desc(weight_tr, CNNL_LAYOUT_ARRAY);
+    cnnlTensorLayout_t layout = CNNL_LAYOUT_NHWC;
+    CnnlTensorDescriptor input_channel_last_desc(input_channel_last, layout);
+    CnnlTensorDescriptor output_channel_last_desc(output_channel_last, layout);
+
+
+    //get workspace for BNFT
+    size_t workspace_size = 0;
+    DIOPI_CHECKCNNL(cnnlGetBatchNormForwardWorkspaceSize(handle, input_channel_last_desc.get(), &workspace_size));
+
+    void* workspace_ptr = workspace_size== 0 ? impl::camb::requiresBuffer(ctx, workspace_size).data() : nullptr;
+
+    // set activition part to default
+    cnnlActivationMode_t active_mode = CNNL_ACTIVATION_IDENTITY;
+    cnnlActivationDescriptor_t activation_desc = nullptr;
+    cnnlCreateActivationDescriptor (&activation_desc);
+    cnnlSetActivationDescriptor_v5(activation_desc, active_mode, CNNL_ACTIVATION_HIGH_PRECISION,
+                                                        CNNL_NOT_PROPAGATE_NAN, 1.0, -1, 1.0, 1.0, false);
+
+    if (training) {
+        DIOPI_CALLCNNL(cnnlBatchNormForwardTraining_v2(
+            /* handle   */ handle,
+            /*activation_desc */ activation_desc,
+            /*mode */ CNNL_BATCHNORM_SPATIAL,
+            /*bnOps */ CNNL_BATCHNORM_OPS_BN,
+            /* alpha    */ nullptr,
+            /* beta     */ nullptr,
+            /* x_desc   */ input_channel_last_desc.get(),
+            /* x        */ input_channel_last.data(),
+            /* z_desc */ NULL,
+            /* z */ NULL,
+            /* wbmvd    */ weight_bias_mean_var_desc.get(),
+            /* weight   */ weight_tr.data(),
+            /* bias     */ bias_tr.data(),
+            /* mov_mean */ running_mean_tr.defined() ? running_mean_tr.data() : nullptr,
+            /* mov_var  */ running_var_tr.defined() ? running_var_tr.data() : nullptr,
+            /* eps      */ static_cast<float>(eps),
+            /* momentum */ static_cast<float>(momentum),
+            /* y_desc   */ output_channel_last_desc.get(),
+            /* y        */ output_channel_last.data(),
+            /* save_mean*/ save_mean_tr.data(),
+            /* save_std */ save_invstd_tr.data(),
+            /* workspace */ workspace_ptr,
+            /* workspace_size */ workspace_size,
+            /* reservespace */ NULL,
+            /* reservespace_size */ 0));
+    } else {
+        DIOPI_CALLCNNL(cnnlBatchNormForwardInference(
+            /* handle   */ handle,
+            /* alpha    */ nullptr,
+            /* beta     */ nullptr,
+            /* x_desc   */ input_channel_last_desc.get(),
+            /* x        */ input_channel_last.data(),
+            /* wbmvd    */ weight_bias_mean_var_desc.get(),
+            /* weight   */ weight_tr.data(),
+            /* bias     */ bias_tr.data(),
+            /* mov_mean */ running_mean_tr.defined() ? running_mean_tr.data() : nullptr,
+            /* mov_var  */ running_var_tr.defined() ? running_var_tr.data() : nullptr,
+            /* eps      */ static_cast<float>(eps),
+            /* z_desc   */ output_channel_last_desc.get(),
+            /* z        */ output_channel_last.data()));
+    }
+
+    // NHWC -> NCHW
+    cnnl_transpose(ctx, handle, output_channel_last, output_tr, CNNL_LAYOUT_NHWC, CNNL_LAYOUT_NCHW);
+
+    // cnrtQueueSync(stream);
+    return diopiSuccess;
+}
+
+diopiError_t diopiBatchNormBackward(diopiContextHandle_t ctx, diopiTensorHandle_t grad_input, diopiTensorHandle_t grad_weight,
+                                              diopiTensorHandle_t grad_bias, diopiConstTensorHandle_t grad_output, diopiConstTensorHandle_t input, diopiConstTensorHandle_t weight,
+                                              diopiConstTensorHandle_t running_mean, diopiConstTensorHandle_t running_var, diopiConstTensorHandle_t save_mean,
+                                              diopiConstTensorHandle_t save_invstd, bool training, double eps){
+    /* Generate diopi Stream, Tensors and Handle*/
+    auto stream  = impl::camb::getStream(ctx);
+
+    auto grad_input_tr = impl::camb::makeTensor(grad_input);
+    auto grad_weight_tr = impl::camb::makeTensor(grad_weight);
+    auto grad_bias_tr = impl::camb::makeTensor(grad_bias);
+    auto input_tr = impl::camb::makeTensor(input);
+    auto weight_tr = impl::camb::makeTensor(weight);
+    auto running_mean_tr = impl::camb::makeTensor(running_mean);
+    auto running_var_tr = impl::camb::makeTensor(running_var);
+    auto save_mean_tr = impl::camb::makeTensor(save_mean);
+    auto save_invstd_tr = impl::camb::makeTensor(save_invstd);
+
+    auto grad_out_tr = impl::camb::makeTensor(grad_output);
+
+    CnnlResourceGuard<cnnlHandle_t, cnnlCreate, cnnlDestroy> CnnlHandle;
+    cnnlHandle_t handle = CnnlHandle.get();
+    DIOPI_CALLCNNL(cnnlSetQueue(handle, stream));
+
+    /* Some basic check */
+    DIOPI_CHECK(running_mean_tr.dtype() ==  running_var_tr.dtype(), "running_mean and running_var need to have the same data types");
+
+    // /* Generate description */
+    // cnnlTensorDescriptor weight_bias_mean_var_desc, input_desc, grad_output_desc, grad_input_desc;
+
+    // // When input is 2D or 3D Tensor, Reshape it to 4D tensor
+    // // NC -> NC11 -> NCHW | NCD -> NCD1 -> NCHW
+    // if (input_dim == 3 || input_dim == 2) {
+
+    //     std::vector<int> input_shape{1, 1, 1, 1};
+    //     std::vector<int> grad_out_shape{1, 1, 1, 1};
+    //     std::vector<int> grad_input_shape{1, 1, 1, 1};
+    //     for (int i = 0; i < input_dim; ++i) {
+    //         input_shape[i] = input_tr.shape().data[i];
+    //         grad_out_shape[i] = grad_out_tr.shape().data[i];
+    //         grad_input_shape[i] = grad_input_tr.shape().data[i];
+    //     }
+    //     // set cnnl descriptor
+    //     input_desc.set_additional_dim(input_tr, input_shape);
+    //     grad_output_desc.set_additional_dim(grad_out_tr, grad_out_shape);
+    //     grad_input_desc.set_additional_dim(grad_input_tr, grad_input_shape);
+    //     weight_bias_mean_var_desc.set(weight_tr, CNNL_LAYOUT_ARRAY);
+    // } else {
+    //     cnnlTensorLayout_t layout =
+    //         input_dim > 4 ? CNNL_LAYOUT_NDHWC : CNNL_LAYOUT_NHWC;
+    //     // set cnnl descriptor
+    //     input_desc.set(input_tr, layout);
+    //     grad_output_desc.set(grad_out_tr, layout);
+    //     grad_input_desc.set(grad_input_tr, layout);
+    //     weight_bias_mean_var_desc.set(weight_tr, CNNL_LAYOUT_ARRAY);
+    // }
+
+    // if (training) {
+    //     DIOPI_CALLCNNL(cnnlBatchNormBackward(
+    //         /* handle           */ handle,
+    //         /* alpha_data_diff  */ nullptr,
+    //         /* beta_data_diff   */ nullptr,
+    //         /* alpha_param_diff */ nullptr,
+    //         /* beta_param_diff  */ nullptr,
+    //         /* x_desc           */ input_desc.get(),
+    //         /* input                */ input_tr.data(),
+    //         /* diff_z_desc      */ grad_output_desc.get(),
+    //         /* diff_z           */ grad_out_tr.data(),
+    //         /* wbmv_desc        */ weight_bias_mean_var_desc.get(),
+    //         /* weight           */ weight_tr.data(),
+    //         /* saved_mean       */ save_mean_tr.data(),
+    //         /* saved_invstd     */ save_invstd_tr.data(),
+    //         /* eps              */ static_cast<float>(eps),
+    //         /* grad_input_desc    */ grad_input_desc.get(),
+    //         /* diff_x           */ grad_input_tr.data(),
+    //         /* diff_weight      */ grad_weight_tr.data(),
+    //         /* diff_bias        */ grad_bias_tr.data()));
+    // } else {
+    //     DIOPI_CALLCNNL(cnnlFrozenBatchNormBackward(
+    //         /* handle       */  handle,
+    //         /* x_desc       */  input_desc.get(),
+    //         /* input            */  input_tr.data(),
+    //         /* diff_y_desc  */  grad_output_desc.get(),
+    //         /* diff_y       */  grad_out_tr.data(),
+    //         /* wbmv_desc    */  weight_bias_mean_var_desc.get(),
+    //         /* weight       */  weight_tr.data(),
+    //         /* pop_mean     */  running_mean_tr.data(),
+    //         /* pop_var      */  running_var_tr.data(),
+    //         /* eps          */  static_cast<float>(eps),
+    //         /* grad_input_desc */ grad_input_desc.get(),
+    //         /* diff_x        */ grad_input_tr.data(),
+    //         /* diff_weight   */ grad_weight_tr.data(),
+    //         /* diff_bias     */ grad_bias_tr.data()));
+    // }
+    // cnrtQueueSync(stream);
     return diopiSuccess;
 }
 
--- camb/helper.hpp
+++ camb/helper.hpp
@@ -11,6 +11,8 @@
 #include <diopi/diopirt.h>
 #include <cnnl.h>
 #include <utility>
+#include <vector>
+#include <iostream>
 
 
 #define DIOPI_CALL(Expr) {                                                              \
@@ -23,6 +25,13 @@
 namespace impl {
 
 namespace camb {
+
+enum class MemoryFormat : size_t {
+    Contiguous      = 0,
+    ChannelsLast    = 1,
+    ChannelsLast3d  = 2,
+    Preserve        = 3
+};
 
 template<typename TensorType>
 struct DataType;
@@ -47,11 +56,28 @@ struct DataType<const diopiTensorHandle_t> {
     }
 };
 
+template<>
+struct DataType<diopiConstTensorHandle_t> {
+    using type = const void*;
+
+    static const void* data(diopiConstTensorHandle_t& tensor) {
+        const void *data;
+        diopiGetTensorDataConst(&tensor, &data);
+        return data;
+    }
+};
+
 template<typename TensorType>
 class DiopiTensor final {
 public:
     explicit DiopiTensor(TensorType& tensor) : tensor_(tensor) {}
 
+    // TODO: 询问能否获取context（需要改header），这样contiguous就不用传ctx了
+    // diopiContextHandle_t context() const {
+    //     diopiContextHandle_t context;
+    //     _diopiTensorGetCtxHandle(tensor_, &context);
+    //     return context;
+    // }
     diopiDevice_t device() const {
         diopiDevice_t device;
         diopiGetTensorDevice(tensor_, &device);
@@ -81,6 +107,61 @@ public:
         int64_t elemsize;
         diopiGetTensorElemSize(tensor_, &elemsize);
         return elemsize;
+    }
+    int64_t dim() {
+        return this->shape().len;
+    }
+    bool defined() const {
+        return this->numel() != 0;
+    }
+
+    DiopiTensor unsqueeze(int dim) {
+        // TODO
+        return *this;
+    }
+    DiopiTensor squeeze(int dim) {
+        // TODO
+        return *this;
+    }
+    DiopiTensor<diopiTensorHandle_t> contiguous(diopiContextHandle_t ctx, impl::camb::MemoryFormat format) {
+        /* Returns a new Tensor in new memory format, without data copy */
+        size_t dim = this->dim();
+        std::vector<int64_t> strides(dim);
+        int64_t stride = 1;
+        auto shapes = this->shape().data;
+        if (format == impl::camb::MemoryFormat::Contiguous) {
+            for (size_t i = dim; i > 0; --i) {
+                strides[i - 1] = stride;
+                if (shapes[i - 1] == 0) continue;
+                if (shapes[i - 1] == -1) stride = -1;
+                if (stride != -1) stride *= shapes[i - 1];
+            }
+        } else if (format == impl::camb::MemoryFormat::ChannelsLast) {
+            for (auto k : {1, 3, 2, 0}) {
+                strides[k] = stride;
+                if (shapes[k] == 0) continue;
+                if (shapes[k] == -1) stride = -1;
+                if (stride != -1) stride *= shapes[k];
+            }
+        } 
+        diopiSize_t diopi_stride(strides.data(), static_cast<int64_t>(strides.size()));
+        diopiTensorHandle_t tensor;
+        diopiRequireTensor(ctx, &tensor, &this->shape(), &diopi_stride, this->dtype(), this->device());
+        return DiopiTensor<diopiTensorHandle_t>(tensor);
+    }
+    void print_str() {
+        int dim = this->dim();
+        std::cout << "DiopiTensor: dim " << dim << ", shape: [";
+        for (size_t i = 0; i < dim; i++)
+        {
+            std::cout << this->shape().data[i] << ", ";
+        }
+        std::cout << "], stride: [";
+        for (size_t i = 0; i < dim; i++)
+        {
+            std::cout << this->stride().data[i] << ", ";
+        }
+        std::cout << "] pointer address: " << this->data() << std::endl;
     }
 
     typename DataType<TensorType>::type data() {
