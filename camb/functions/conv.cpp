c0df6c51aab48f0416c46810aa2f212e9897a2bc
zhangzefeng <zhang_zefeng@foxmail.com>
2023-03-01 09:59:24 +0800

    format c++ code

--- camb/functions/conv.cpp
+++ camb/functions/conv.cpp
@@ -1,23 +1,18 @@
-#include <vector>
-
 #include <diopi/functions.h>
+
+#include <vector>
 
 #include "../cnnl_helper.hpp"
 
 extern "C" {
 
-static diopiError_t diopiTransposeNCHW2NHWC(diopiContextHandle_t ctx,
-                                            diopiConstTensorHandle_t input,
-                                            diopiTensorHandle_t out) {
+static diopiError_t diopiTransposeNCHW2NHWC(diopiContextHandle_t ctx, diopiConstTensorHandle_t input, diopiTensorHandle_t out) {
     auto stream = impl::camb::getStream(ctx);
     CnnlResourceGuard<cnnlHandle_t, cnnlCreate, cnnlDestroy> CnnlHandle;
     cnnlHandle_t handle = CnnlHandle.get();
     DIOPI_CALLCNNL(cnnlSetQueue(handle, stream));
 
-    CnnlResourceGuard<cnnlTensorDescriptor_t,
-                      cnnlCreateTensorDescriptor,
-                      cnnlDestroyTensorDescriptor>
-        inputDesc, outDesc;
+    CnnlResourceGuard<cnnlTensorDescriptor_t, cnnlCreateTensorDescriptor, cnnlDestroyTensorDescriptor> inputDesc, outDesc;
     cnnlTensorDescriptor_t input_desc = inputDesc.get();
     cnnlTensorDescriptor_t out_desc = outDesc.get();
 
@@ -37,29 +32,16 @@ static diopiError_t diopiTransposeNCHW2NHWC(diopiContextHandle_t ctx,
         out_dims[i] = input_shape.data[i];
         input_stride[i] = inputStride.data[i];
     }
-    DIOPI_CALLCNNL(cnnlSetTensorDescriptorEx(input_desc,
-                                             CNNL_LAYOUT_NCHW,
-                                             cnnl_dtype,
-                                             input_shape.len,
-                                             input_dims.data(),
-                                             input_stride.data()));
+    DIOPI_CALLCNNL(cnnlSetTensorDescriptorEx(input_desc, CNNL_LAYOUT_NCHW, cnnl_dtype, input_shape.len, input_dims.data(), input_stride.data()));
     out_dims[1] = input_dims[2];
     out_dims[2] = input_dims[3];
     out_dims[3] = input_dims[1];
-    DIOPI_CALLCNNL(cnnlSetTensorDescriptor(out_desc,
-                                           CNNL_LAYOUT_NHWC,
-                                           cnnl_dtype,
-                                           input_shape.len,
-                                           out_dims.data()));
+    DIOPI_CALLCNNL(cnnlSetTensorDescriptor(out_desc, CNNL_LAYOUT_NHWC, cnnl_dtype, input_shape.len, out_dims.data()));
 
-    CnnlResourceGuard<cnnlTransposeDescriptor_t,
-                      cnnlCreateTransposeDescriptor,
-                      cnnlDestroyTransposeDescriptor>
-        transDesc;
+    CnnlResourceGuard<cnnlTransposeDescriptor_t, cnnlCreateTransposeDescriptor, cnnlDestroyTransposeDescriptor> transDesc;
     cnnlTransposeDescriptor_t trans_desc = transDesc.get();
     std::vector<int> order = {0, 2, 3, 1};
-    DIOPI_CALLCNNL(
-        cnnlSetTransposeDescriptor(trans_desc, input_shape.len, order.data()));
+    DIOPI_CALLCNNL(cnnlSetTransposeDescriptor(trans_desc, input_shape.len, order.data()));
 
     const void* input_data = nullptr;
     void* out_data = nullptr;
@@ -67,33 +49,20 @@ static diopiError_t diopiTransposeNCHW2NHWC(diopiContextHandle_t ctx,
     diopiGetTensorData(&out, &out_data);
 
     size_t workspace_size = 0;
-    DIOPI_CALLCNNL(cnnlGetTransposeWorkspaceSize(
-        handle, input_desc, trans_desc, &workspace_size));
+    DIOPI_CALLCNNL(cnnlGetTransposeWorkspaceSize(handle, input_desc, trans_desc, &workspace_size));
     auto workspace = impl::camb::requiresBuffer(ctx, workspace_size);
 
-    DIOPI_CALLCNNL(cnnlTranspose_v2(handle,
-                                    trans_desc,
-                                    input_desc,
-                                    input_data,
-                                    out_desc,
-                                    out_data,
-                                    workspace.data(),
-                                    workspace_size));
+    DIOPI_CALLCNNL(cnnlTranspose_v2(handle, trans_desc, input_desc, input_data, out_desc, out_data, workspace.data(), workspace_size));
     return diopiSuccess;
 }
 
-static diopiError_t diopiTransposeNHWC2NCHW(diopiContextHandle_t ctx,
-                                            diopiConstTensorHandle_t input,
-                                            diopiTensorHandle_t out) {
+static diopiError_t diopiTransposeNHWC2NCHW(diopiContextHandle_t ctx, diopiConstTensorHandle_t input, diopiTensorHandle_t out) {
     auto stream = impl::camb::getStream(ctx);
     CnnlResourceGuard<cnnlHandle_t, cnnlCreate, cnnlDestroy> CnnlHandle;
     cnnlHandle_t handle = CnnlHandle.get();
     DIOPI_CALLCNNL(cnnlSetQueue(handle, stream));
 
-    CnnlResourceGuard<cnnlTensorDescriptor_t,
-                      cnnlCreateTensorDescriptor,
-                      cnnlDestroyTensorDescriptor>
-        inputDesc, outDesc;
+    CnnlResourceGuard<cnnlTensorDescriptor_t, cnnlCreateTensorDescriptor, cnnlDestroyTensorDescriptor> inputDesc, outDesc;
     cnnlTensorDescriptor_t input_desc = inputDesc.get();
     cnnlTensorDescriptor_t out_desc = outDesc.get();
 
@@ -113,29 +82,16 @@ static diopiError_t diopiTransposeNHWC2NCHW(diopiContextHandle_t ctx,
         out_dims[i] = input_shape.data[i];
         input_stride[i] = inputStride.data[i];
     }
-    DIOPI_CALLCNNL(cnnlSetTensorDescriptorEx(input_desc,
-                                             CNNL_LAYOUT_NHWC,
-                                             cnnl_dtype,
-                                             input_shape.len,
-                                             input_dims.data(),
-                                             input_stride.data()));
+    DIOPI_CALLCNNL(cnnlSetTensorDescriptorEx(input_desc, CNNL_LAYOUT_NHWC, cnnl_dtype, input_shape.len, input_dims.data(), input_stride.data()));
     out_dims[1] = input_dims[3];
     out_dims[2] = input_dims[1];
     out_dims[3] = input_dims[2];
-    DIOPI_CALLCNNL(cnnlSetTensorDescriptor(out_desc,
-                                           CNNL_LAYOUT_NCHW,
-                                           cnnl_dtype,
-                                           input_shape.len,
-                                           out_dims.data()));
+    DIOPI_CALLCNNL(cnnlSetTensorDescriptor(out_desc, CNNL_LAYOUT_NCHW, cnnl_dtype, input_shape.len, out_dims.data()));
 
-    CnnlResourceGuard<cnnlTransposeDescriptor_t,
-                      cnnlCreateTransposeDescriptor,
-                      cnnlDestroyTransposeDescriptor>
-        transDesc;
+    CnnlResourceGuard<cnnlTransposeDescriptor_t, cnnlCreateTransposeDescriptor, cnnlDestroyTransposeDescriptor> transDesc;
     cnnlTransposeDescriptor_t trans_desc = transDesc.get();
     std::vector<int> order = {0, 3, 1, 2};
-    DIOPI_CALLCNNL(
-        cnnlSetTransposeDescriptor(trans_desc, input_shape.len, order.data()));
+    DIOPI_CALLCNNL(cnnlSetTransposeDescriptor(trans_desc, input_shape.len, order.data()));
 
     const void* input_data = nullptr;
     void* out_data = nullptr;
@@ -143,23 +99,14 @@ static diopiError_t diopiTransposeNHWC2NCHW(diopiContextHandle_t ctx,
     diopiGetTensorData(&out, &out_data);
 
     size_t workspace_size = 0;
-    DIOPI_CALLCNNL(cnnlGetTransposeWorkspaceSize(
-        handle, input_desc, trans_desc, &workspace_size));
+    DIOPI_CALLCNNL(cnnlGetTransposeWorkspaceSize(handle, input_desc, trans_desc, &workspace_size));
     auto workspace = impl::camb::requiresBuffer(ctx, workspace_size);
 
-    DIOPI_CALLCNNL(cnnlTranspose_v2(handle,
-                                    trans_desc,
-                                    input_desc,
-                                    input_data,
-                                    out_desc,
-                                    out_data,
-                                    workspace.data(),
-                                    workspace_size));
+    DIOPI_CALLCNNL(cnnlTranspose_v2(handle, trans_desc, input_desc, input_data, out_desc, out_data, workspace.data(), workspace_size));
     return diopiSuccess;
 }
 
-static diopiTensorHandle_t diopiRequireNCHW2NHWC(
-    diopiContextHandle_t ctx, diopiConstTensorHandle_t input) {
+static diopiTensorHandle_t diopiRequireNCHW2NHWC(diopiContextHandle_t ctx, diopiConstTensorHandle_t input) {
     diopiDtype_t dtype;
     diopiGetTensorDtype(input, &dtype);
     diopiSize_t nchw_shape;
@@ -176,12 +123,8 @@ static diopiTensorHandle_t diopiRequireNCHW2NHWC(
     return out;
 }
 
-static diopiError_t diopiPrepareConv2dDesc(cnnlConvolutionDescriptor_t desc,
-                                           diopiConstTensorHandle_t input,
-                                           diopiSize_t stride,
-                                           diopiSize_t padding,
-                                           diopiSize_t dilation,
-                                           int64_t groups) {
+static diopiError_t diopiPrepareConv2dDesc(
+    cnnlConvolutionDescriptor_t desc, diopiConstTensorHandle_t input, diopiSize_t stride, diopiSize_t padding, diopiSize_t dilation, int64_t groups) {
     std::vector<int> conv_padding(4);
     if (4 == padding.len) {
         for (int i = 0; i < 4; i++) {
@@ -212,62 +155,22 @@ static diopiError_t diopiPrepareConv2dDesc(cnnlConvolutionDescriptor_t desc,
     diopiDtype_t diopi_dtype;
     diopiGetTensorDtype(input, &diopi_dtype);
     DIOPI_CALL(convertType(&cnnl_dtype, diopi_dtype));
-    DIOPI_CALLCNNL(cnnlSetConvolutionDescriptor(desc,
-                                                input_shape.len,
-                                                conv_padding.data(),
-                                                conv_stride.data(),
-                                                conv_dilation.data(),
-                                                groups,
-                                                cnnl_dtype));
+    DIOPI_CALLCNNL(cnnlSetConvolutionDescriptor(desc, input_shape.len, conv_padding.data(), conv_stride.data(), conv_dilation.data(), groups, cnnl_dtype));
     return diopiSuccess;
 }
 
-static diopiError_t diopiGetNHWCDescriptor(diopiConstTensorHandle_t input,
-                                           cnnlTensorDescriptor_t desc) {
-    diopiDtype_t diopi_dtype;
-    cnnlDataType_t cnnl_dtype;
-    diopiGetTensorDtype(input, &diopi_dtype);
-    convertType(&cnnl_dtype, diopi_dtype);
-    diopiSize_t input_shape, input_stride;
-    diopiGetTensorShape(input, &input_shape);
-    diopiGetTensorStride(input, &input_stride);
-    std::vector<int> shape(input_shape.len), stride(input_stride.len);
-    for (int i = 0; i < input_shape.len; i++) {
-        shape[i] = input_shape.data[i];
-    }
-    for (int i = 0; i < input_stride.len; i++) {
-        stride[i] = input_stride.data[i];
-    }
-
-    cnnlTensorLayout_t layout =
-        (4 == input_shape.len) ? CNNL_LAYOUT_NHWC : CNNL_LAYOUT_ARRAY;
-    DIOPI_CALLCNNL(cnnlSetTensorDescriptorEx(desc,
-                                             layout,
-                                             cnnl_dtype,
-                                             input_shape.len,
-                                             shape.data(),
-                                             stride.data()));
-    return diopiSuccess;
-}
-
-static diopiTensorHandle_t diopiRequireNCHWConv2dOut(
-    diopiContextHandle_t ctx,
-    cnnlConvolutionDescriptor_t conv_desc,
-    diopiConstTensorHandle_t input,
-    diopiConstTensorHandle_t weight) {
-    CnnlResourceGuard<cnnlTensorDescriptor_t,
-                      cnnlCreateTensorDescriptor,
-                      cnnlDestroyTensorDescriptor>
-        inputDesc, weightDesc;
-    cnnlTensorDescriptor_t input_desc = inputDesc.get();
-    cnnlTensorDescriptor_t weight_desc = weightDesc.get();
-    diopiGetNHWCDescriptor(input, input_desc);
-    diopiGetNHWCDescriptor(weight, weight_desc);
+static diopiTensorHandle_t diopiRequireNCHWConv2dOut(diopiContextHandle_t ctx,
+                                                     cnnlConvolutionDescriptor_t conv_desc,
+                                                     diopiConstTensorHandle_t input,
+                                                     diopiConstTensorHandle_t weight) {
+    auto at_input = impl::camb::makeTensor(input);
+    auto at_weight = impl::camb::makeTensor(weight);
+    CnnlTensorDesc input_desc(at_input, CNNL_LAYOUT_NHWC);
+    CnnlTensorDesc weight_desc(at_weight, CNNL_LAYOUT_NHWC);
 
     std::vector<int> out_shape(4);
     std::vector<int64_t> out_shape64(4);
-    cnnlGetConvolutionForwardOutputDim(
-        conv_desc, input_desc, weight_desc, 4, out_shape.data());
+    cnnlGetConvolutionForwardOutputDim(conv_desc, input_desc.get(), weight_desc.get(), 4, out_shape.data());
     for (int i = 0; i < 4; i++) {
         out_shape64[i] = out_shape[i];
     }
@@ -294,54 +197,35 @@ diopiError_t diopiConvolution2d(diopiContextHandle_t ctx,
     cnnlHandle_t handle = CnnlHandle.get();
     DIOPI_CALLCNNL(cnnlSetQueue(handle, stream));
 
-    CnnlResourceGuard<cnnlConvolutionDescriptor_t,
-                      cnnlCreateConvolutionDescriptor,
-                      cnnlDestroyConvolutionDescriptor>
-        convDesc;
+    CnnlResourceGuard<cnnlConvolutionDescriptor_t, cnnlCreateConvolutionDescriptor, cnnlDestroyConvolutionDescriptor> convDesc;
     cnnlConvolutionDescriptor_t conv_desc = convDesc.get();
     diopiPrepareConv2dDesc(conv_desc, input, stride, padding, dilation, groups);
 
     diopiConstTensorHandle_t nhwc_input = diopiRequireNCHW2NHWC(ctx, input);
     diopiConstTensorHandle_t nhwc_weight = diopiRequireNCHW2NHWC(ctx, weight);
-    diopiTensorHandle_t nhwc_out =
-        diopiRequireNCHWConv2dOut(ctx, conv_desc, nhwc_input, nhwc_weight);
+    diopiTensorHandle_t nhwc_out = diopiRequireNCHWConv2dOut(ctx, conv_desc, nhwc_input, nhwc_weight);
+    auto at_nhwc_input = impl::camb::makeTensor(nhwc_input);
+    auto at_nhwc_weight = impl::camb::makeTensor(nhwc_weight);
+    auto at_nhwc_out = impl::camb::makeTensor(nhwc_out);
 
-    CnnlResourceGuard<cnnlTensorDescriptor_t,
-                      cnnlCreateTensorDescriptor,
-                      cnnlDestroyTensorDescriptor>
-        inputDesc, weightDesc, biasDesc, outDesc;
-    cnnlTensorDescriptor_t input_desc = inputDesc.get();
-    cnnlTensorDescriptor_t weight_desc = weightDesc.get();
-    cnnlTensorDescriptor_t bias_desc = biasDesc.get();
-    cnnlTensorDescriptor_t out_desc = outDesc.get();
-    diopiGetNHWCDescriptor(nhwc_input, input_desc);
-    diopiGetNHWCDescriptor(nhwc_weight, weight_desc);
-    if (nullptr == bias) {
-        bias_desc = nullptr;
-    } else {
-        diopiGetNHWCDescriptor(bias, bias_desc);
+    CnnlTensorDesc input_desc(at_nhwc_input, CNNL_LAYOUT_NHWC);
+    CnnlTensorDesc weight_desc(at_nhwc_weight, CNNL_LAYOUT_NHWC);
+    CnnlTensorDesc out_desc(at_nhwc_out, CNNL_LAYOUT_NHWC);
+    cnnlTensorDescriptor_t bias_descriptor = nullptr;
+    CnnlTensorDesc bias_desc;
+    if (nullptr != bias) {
+        auto at_nhwc_bias = impl::camb::makeTensor(bias);
+        bias_desc.set(at_nhwc_bias, CNNL_LAYOUT_ARRAY);
+        bias_descriptor = bias_desc.get();
     }
-    diopiGetNHWCDescriptor(nhwc_out, out_desc);
 
     cnnlConvolutionForwardAlgo_t algo;
     DIOPI_CALLCNNL(
-        cnnlGetConvolutionForwardAlgorithm(handle,
-                                           conv_desc,
-                                           input_desc,
-                                           weight_desc,
-                                           out_desc,
-                                           CNNL_CONVOLUTION_FWD_FASTEST,
-                                           &algo));
+        cnnlGetConvolutionForwardAlgorithm(handle, conv_desc, input_desc.get(), weight_desc.get(), out_desc.get(), CNNL_CONVOLUTION_FWD_FASTEST, &algo));
 
     size_t workspace_size(0);
-    DIOPI_CALLCNNL(cnnlGetConvolutionForwardWorkspaceSize(handle,
-                                                          input_desc,
-                                                          weight_desc,
-                                                          out_desc,
-                                                          bias_desc,
-                                                          conv_desc,
-                                                          algo,
-                                                          &workspace_size));
+    DIOPI_CALLCNNL(
+        cnnlGetConvolutionForwardWorkspaceSize(handle, input_desc.get(), weight_desc.get(), out_desc.get(), bias_descriptor, conv_desc, algo, &workspace_size));
     auto workspace = impl::camb::requiresBuffer(ctx, workspace_size);
 
     const void* input_data = nullptr;
@@ -349,9 +233,7 @@ diopiError_t diopiConvolution2d(diopiContextHandle_t ctx,
     const void* weight_data = nullptr;
     diopiGetTensorDataConst(&nhwc_weight, &weight_data);
     const void* bias_data = nullptr;
-    if (nullptr == bias) {
-        bias_data = nullptr;
-    } else {
+    if (nullptr != bias) {
         diopiGetTensorDataConst(&bias, &bias_data);
     }
     void* out_data = nullptr;
@@ -361,16 +243,16 @@ diopiError_t diopiConvolution2d(diopiContextHandle_t ctx,
                                           conv_desc,
                                           algo,
                                           nullptr,
-                                          input_desc,
+                                          input_desc.get(),
                                           input_data,
-                                          weight_desc,
+                                          weight_desc.get(),
                                           weight_data,
-                                          bias_desc,
+                                          bias_descriptor,
                                           bias_data,
                                           workspace.data(),
                                           workspace_size,
                                           nullptr,
-                                          out_desc,
+                                          out_desc.get(),
                                           out_data));
     diopiTransposeNHWC2NCHW(ctx, nhwc_out, out);
     return diopiSuccess;
