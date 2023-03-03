cb43f69e5fa6f0455a61661e97ae6014594b9789
yehongjuan <yehongjuan@cambricon.com>
2023-03-03 08:00:39 +0000

    fix mean

--- camb/functions/mean.cpp
+++ camb/functions/mean.cpp
@@ -3,37 +3,22 @@
 #include "../cnnl_helper.hpp"
 
 namespace {
-std::vector<int> getAxis(diopiConstTensorHandle_t input, diopiSize_t dim) {
-    std::vector<int> axis;
-    if (dim.len > 0) {
-        for (int i = 0; i < dim.len; i++) {
-            axis.push_back(static_cast<int>(dim.data[i]));
-        }
-    } else {
-        auto input_tensor = impl::camb::makeTensor(input);
-        int input_size = input_tensor.shape().size();
-        for (int i = 0; i < input_size; i++) {
-            axis.push_back(i);
-        }
+void getAxis(auto& t, std::vector<int>& axis) {
+    axis.clear();
+    int input_size = t.shape().size();
+    for (int i = 0; i < input_size; i++) {
+        axis.push_back(i);
     }
-
-    return axis;
 }
 }  // namespace
 
 extern "C" {
 
 DIOPI_API diopiError_t diopiMean(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, diopiSize_t dim, diopiDtype_t dtype) {
-    auto stream = impl::camb::getStream(ctx);
-    CnnlResourceGuard<cnnlHandle_t, cnnlCreate, cnnlDestroy> CnnlHandle;
-    cnnlHandle_t handle = CnnlHandle.get();
-    DIOPI_CALLCNNL(cnnlSetQueue(handle, stream));
+    cnnlHandle_t handle = cnnlHandlePool.get(ctx);
 
     auto input_tensor = impl::camb::makeTensor(input);
     auto out_tensor = impl::camb::makeTensor(out);
-    if (input_tensor.dtype() == diopi_dtype_float64) {
-        return diopiDtypeNotSupported;
-    }
 
     CnnlTensorDesc input_desc(input_tensor, CNNL_LAYOUT_ARRAY);
     CnnlTensorDesc out_desc;
@@ -50,7 +35,13 @@ DIOPI_API diopiError_t diopiMean(diopiContextHandle_t ctx, diopiTensorHandle_t o
     cnnlReduceDescriptor_t reduce_desc = CnnlReduceDesc.get();
     cnnlDataType_t cnnl_dtype;
     DIOPI_CALL(convertType(&cnnl_dtype, dtype));
-    std::vector<int> axis = getAxis(input, dim);
+    std::vector<int> axis;
+    if (dim.len > 0) {
+        std::vector<int> dims{dim.data, dim.data + dim.len};
+        axis = dims;
+    } else {
+        getAxis(input_tensor, axis);
+    }
     DIOPI_CALLCNNL(cnnlSetReduceDescriptor(
         reduce_desc, axis.data(), axis.size(), CNNL_REDUCE_AVG, cnnl_dtype, CNNL_NOT_PROPAGATE_NAN, CNNL_REDUCE_NO_INDICES, CNNL_32BIT_INDICES));
     size_t workspace_size = 0;
@@ -62,5 +53,6 @@ DIOPI_API diopiError_t diopiMean(diopiContextHandle_t ctx, diopiTensorHandle_t o
 
     DIOPI_CALLCNNL(
         cnnlReduce(handle, reduce_desc, workspace, workspace_size, nullptr, input_desc.get(), input_ptr, 0, nullptr, nullptr, out_desc.get(), out_ptr));
+    return diopiSuccess;
 }
 }
