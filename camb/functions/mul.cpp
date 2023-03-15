948c20ab8370e9e4e10539392bb7f4e6b26dfbc4
zhangqiu <zhangqiu@sensetime.com>
2023-03-15 21:24:55 +0800

    fix varName: other_t to other_temp

--- camb/functions/mul.cpp
+++ camb/functions/mul.cpp
@@ -12,20 +12,16 @@ using DiopiTensorT = DiopiTensor<diopiTensorHandle_t>;
 diopiError_t broadcast(diopiContextHandle_t ctx, DiopiTensorT& out, const DiopiTensorT& input) {
     cnnlHandle_t handle = cnnlHandlePool.get(ctx);
     // check whether input.shape() match the targetShape
-    std::vector<int32_t> targetShape(out.shape().begin(), out.shape().end());
-    std::vector<int32_t> inputShape(input.shape().begin(), input.shape().end());
-
-    int32_t nDimsTarget = targetShape.size();
-    int32_t nDimsInput = inputShape.size();
+    std::vector<int64_t> targetShape = out.shape();
+    std::vector<int64_t> inputShape = input.shape();
+    int64_t nDimsTarget = targetShape.size();
+    int64_t nDimsInput = inputShape.size();
     if (nDimsInput < nDimsTarget) {
         inputShape.insert(inputShape.begin(), nDimsTarget - nDimsInput, 1);
     }
 
     for (int i = 0; i < nDimsTarget; i++) {
-        if ((inputShape[i] != 1) && (inputShape[i] != targetShape[i])) {
-            DIOPI_CHECK(false, "shape1 not match shape2, can't broadcast");
-            break;
-        }
+        DIOPI_CHECK( ((inputShape[i] == 1) || (inputShape[i] == targetShape[i])), "shape1 not match shape2, can't broadcast");
     }
     CnnlTensorDesc inputDesc(input, CNNL_LAYOUT_ARRAY);
     CnnlTensorDesc outDesc(out, CNNL_LAYOUT_ARRAY);
@@ -52,7 +48,7 @@ DIOPI_API diopiError_t diopiMul(diopiContextHandle_t ctx, diopiTensorHandle_t ou
     diopiTensorHandle_t other_ = diopiTensorHandle_t(other);
 
     auto input_tensor = makeTensor(input_);
-    auto other_tensor = makeTensor(other_);
+    auto other_tempensor = makeTensor(other_);
     auto out_tensor = makeTensor(out);
     DiopiTensor<diopiTensorHandle_t> out_tensor_tmp;
     if ((out_tensor.dtype() != diopi_dtype_float16) && (out_tensor.dtype() != diopi_dtype_float32)) {
@@ -61,17 +57,17 @@ DIOPI_API diopiError_t diopiMul(diopiContextHandle_t ctx, diopiTensorHandle_t ou
         out_tensor_tmp = makeTensor(out);
     }
     input_tensor = dataTypeCast(ctx, input_tensor, out_tensor_tmp.dtype());
-    other_tensor = dataTypeCast(ctx, other_tensor, out_tensor_tmp.dtype());
+    other_tempensor = dataTypeCast(ctx, other_tempensor, out_tensor_tmp.dtype());
 
     DiopiTensorT bcast_input_tensor = broadcastHelper(ctx, input_tensor, out_tensor_tmp);
-    DiopiTensorT bcast_other_tensor = broadcastHelper(ctx, other_tensor, out_tensor_tmp);
+    DiopiTensorT bcast_other_tempensor = broadcastHelper(ctx, other_tempensor, out_tensor_tmp);
 
     CnnlTensorDesc bcast_input_desc(bcast_input_tensor, CNNL_LAYOUT_ARRAY);
-    CnnlTensorDesc bcast_other_desc(bcast_other_tensor, CNNL_LAYOUT_ARRAY);
+    CnnlTensorDesc bcast_other_desc(bcast_other_tempensor, CNNL_LAYOUT_ARRAY);
     CnnlTensorDesc out_desc(out_tensor_tmp, CNNL_LAYOUT_ARRAY);
 
     cnnlTensorDescriptor_t input_descs[] = {bcast_input_desc.get(), bcast_other_desc.get()};
-    const void* inputs[] = {bcast_input_tensor.data(), bcast_other_tensor.data()};
+    const void* inputs[] = {bcast_input_tensor.data(), bcast_other_tempensor.data()};
 
     DIOPI_CALLCNNL(cnnlMulN(handle, input_descs, inputs, 2, out_desc.get(), out_tensor_tmp.data()))
     if (out_tensor_tmp.dtype() != out_tensor.dtype()) {
@@ -90,12 +86,11 @@ DIOPI_API diopiError_t diopiMulScalar(diopiContextHandle_t ctx, diopiTensorHandl
     auto input_tensor = makeTensor(input);
     auto out_tensor = makeTensor(out);
 
-    diopiTensorHandle_t other_t;
-    other_t = requiresTensor(ctx, vec2diopiSize_t(input_tensor.shape()), input_tensor.dtype());
-    DIOPI_CALL(diopiFill(ctx, other_t, other));
-    auto other_t_tensor = makeTensor(other_t);
-    CnnlTensorDesc other_t_desc(other_t_tensor, CNNL_LAYOUT_ARRAY);
-    diopiMul(ctx, out, input, other_t);
+    diopiTensorHandle_t other_temp = requiresTensor(ctx, vec2diopiSize_t(input_tensor.shape()), input_tensor.dtype());
+    DIOPI_CALL(diopiFill(ctx, other_temp, other));
+    auto other_temp_tensor = makeTensor(other_temp);
+    CnnlTensorDesc other_temp_desc(other_temp_tensor, CNNL_LAYOUT_ARRAY);
+    diopiMul(ctx, out, input, other_temp);
     return diopiSuccess;
 }
 
