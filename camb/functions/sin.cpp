a1551f13ca271ef81373535751bd622223c91d49
duanxiang <duanxiang@cambricon.com>
2023-03-16 09:51:28 +0000

    add  abs sin cos

--- camb/functions/sin.cpp
+++ camb/functions/sin.cpp
@@ -1,42 +1,44 @@
 #include <diopi/functions.h>
 
+#include <vector>
+
 #include "../cnnl_helper.hpp"
 #include "../common/common.hpp"
 
 namespace impl {
 namespace camb {
 
-extern "C" diopiError_t diopiAbsInp(diopiContextHandle_t ctx, diopiTensorHandle_t input) {
-    cnnlHandle_t handle = cnnlHandlePool.get(ctx);
+extern "C" diopiError_t diopiSinInp(diopiContextHandle_t ctx, diopiTensorHandle_t input) {
+    auto handle = cnnlHandlePool.get(ctx);
     auto input_tensor = makeTensor(input);
     if (input_tensor.dtype() == diopi_dtype_float64) {
         diopiTensorHandle_t input_ = const_cast<diopiTensorHandle_t>(input);
         DiopiTensorT input_tensor_f32 = dataTypeCast(ctx, makeTensor(input_), diopi_dtype_float32);
         CnnlTensorDesc f32_desc(input_tensor_f32, CNNL_LAYOUT_ARRAY);
-        DIOPI_CALLCNNL(cnnlAbs(handle, f32_desc.get(), input_tensor_f32.data(), f32_desc.get(), input_tensor_f32.data()));
+        DIOPI_CALLCNNL(cnnlSin_v2(handle, CNNL_COMPUTATION_HIGH_PRECISION, f32_desc.get(), input_tensor_f32.data(), f32_desc.get(), input_tensor_f32.data()));
         dataTypeCast(ctx, input_tensor, input_tensor_f32);
+        return diopiDtypeNotSupported;
     } else {
         CnnlTensorDesc input_desc(input_tensor, CNNL_LAYOUT_ARRAY);
-        DIOPI_CALLCNNL(cnnlAbs(handle, input_desc.get(), input_tensor.data(), input_desc.get(), input_tensor.data()));
+        DIOPI_CALLCNNL(cnnlSin_v2(handle, CNNL_COMPUTATION_HIGH_PRECISION, input_desc.get(), input_tensor.data(), input_desc.get(), input_tensor.data()));
     }
     return diopiSuccess;
 }
 
-extern "C" diopiError_t diopiAbs(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input) {
-    cnnlHandle_t handle = cnnlHandlePool.get(ctx);
+extern "C" diopiError_t diopiSin(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input) {
+    auto handle = cnnlHandlePool.get(ctx);
     auto input_tensor = makeTensor(input);
     auto output_tensor = makeTensor(out);
-
     if (input_tensor.dtype() == diopi_dtype_float64) {
         diopiTensorHandle_t input_ = const_cast<diopiTensorHandle_t>(input);
         DiopiTensorT input_tensor_f32 = dataTypeCast(ctx, makeTensor(input_), diopi_dtype_float32);
         CnnlTensorDesc f32_desc(input_tensor_f32, CNNL_LAYOUT_ARRAY);
-        DIOPI_CALLCNNL(cnnlAbs(handle, f32_desc.get(), input_tensor_f32.data(), f32_desc.get(), input_tensor_f32.data()));
+        DIOPI_CALLCNNL(cnnlSin_v2(handle, CNNL_COMPUTATION_HIGH_PRECISION, f32_desc.get(), input_tensor_f32.data(), f32_desc.get(), input_tensor_f32.data()));
         dataTypeCast(ctx, output_tensor, input_tensor_f32);
     } else {
         CnnlTensorDesc input_desc(input_tensor, CNNL_LAYOUT_ARRAY);
         CnnlTensorDesc output_desc(output_tensor, CNNL_LAYOUT_ARRAY);
-        DIOPI_CALLCNNL(cnnlAbs(handle, input_desc.get(), input_tensor.data(), output_desc.get(), output_tensor.data()));
+        DIOPI_CALLCNNL(cnnlSin_v2(handle, CNNL_COMPUTATION_HIGH_PRECISION, input_desc.get(), input_tensor.data(), output_desc.get(), output_tensor.data()));
     }
     return diopiSuccess;
 }
