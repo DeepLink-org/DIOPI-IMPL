94b3c90d9fda7299da2646c26c91fb841ba4b594
zhangqiu <zhangqiu@sensetime.com>
2023-03-17 17:30:51 +0800

    add true_div and reformat broadcast

--- camb/functions/div.cpp
+++ camb/functions/div.cpp
@@ -14,9 +14,19 @@ diopiDiv(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHand
     auto other_tensor = DiopiTensor(other);
     auto out_tensor = DiopiTensor(out);
 
+    DiopiTensor out_tensor_temp;
+    if ((out_tensor.dtype() != diopi_dtype_float16) && (out_tensor.dtype() != diopi_dtype_float32)) {
+        out_tensor_temp = dataTypeCast(ctx, out_tensor, diopi_dtype_float16);
+    } else {
+        out_tensor_temp = DiopiTensor(out);
+    }
+
+    input_tensor = dataTypeCast(ctx, input_tensor, out_tensor_temp.dtype());
+    other_tensor = dataTypeCast(ctx, other_tensor, out_tensor_temp.dtype());
+
     CnnlTensorDesc input_desc(input_tensor, CNNL_LAYOUT_ARRAY);
     CnnlTensorDesc other_desc(other_tensor, CNNL_LAYOUT_ARRAY);
-    CnnlTensorDesc out_desc(out_tensor, CNNL_LAYOUT_ARRAY);
+    CnnlTensorDesc out_desc(out_tensor_temp, CNNL_LAYOUT_ARRAY);
     size_t workspace_size = 0;
     cnnlGetDivWorkspaceSize(handle, input_desc.get(), other_desc.get(), out_desc.get(), &workspace_size);
     void* workspace = nullptr;
