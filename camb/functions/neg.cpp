6d79f21c8284e30fdd23155b5e1afe60be5e6c94
yehongjuan <yehongjuan@cambricon.com>
2023-03-23 08:02:25 +0000

    fix neg

--- camb/functions/neg.cpp
+++ camb/functions/neg.cpp
@@ -10,39 +10,26 @@ namespace camb {
 
 extern "C" {
 
-DIOPI_API diopiError_t diopiNegInp(diopiContextHandle_t ctx, diopiTensorHandle_t input) {
-    cnnlHandle_t handle = cnnlHandlePool.get(ctx);
-    auto input_tensor = DiopiTensor(input);
-    if (input_tensor.dtype() == diopi_dtype_float64) {
-        auto input_cast_tensor = dataTypeCast(ctx, input_tensor, diopi_dtype_float32);
-        CnnlTensorDesc input_cast_desc(input_cast_tensor, CNNL_LAYOUT_ARRAY);
-        DIOPI_CHECKCNNL(cnnlNegTensor(handle, input_cast_desc.get(), input_cast_tensor.data(), input_cast_desc.get(), input_cast_tensor.data()));
-        dataTypeCast(ctx, input_tensor, input_cast_tensor);
-    } else {
-        CnnlTensorDesc input_desc(input_tensor, CNNL_LAYOUT_ARRAY);
-        DIOPI_CHECKCNNL(cnnlNegTensor(handle, input_desc.get(), input_tensor.data(), input_desc.get(), input_tensor.data()));
-    }
-    return diopiSuccess;
-}
-
 DIOPI_API diopiError_t diopiNeg(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input) {
     cnnlHandle_t handle = cnnlHandlePool.get(ctx);
     auto input_tensor = DiopiTensor(input);
     auto out_tensor = DiopiTensor(out);
 
-    if (input_tensor.dtype() == diopi_dtype_float64) {
-        diopiTensorHandle_t input_t = const_cast<diopiTensorHandle_t>(input);
-        auto input_t_tensor = DiopiTensor(input_t);
-        CnnlTensorDesc input_t_desc(input_t_tensor, CNNL_LAYOUT_ARRAY);
-        auto input_cast_tensor = dataTypeCast(ctx, input_t_tensor, diopi_dtype_float32);
-        CnnlTensorDesc input_cast_desc(input_cast_tensor, CNNL_LAYOUT_ARRAY);
-        DIOPI_CHECKCNNL(cnnlNegTensor(handle, input_cast_desc.get(), input_cast_tensor.data(), input_cast_desc.get(), input_cast_tensor.data()));
-        dataTypeCast(ctx, out_tensor, input_cast_tensor);
-    } else {
-        CnnlTensorDesc input_desc(input_tensor, CNNL_LAYOUT_ARRAY);
-        CnnlTensorDesc out_desc(out_tensor, CNNL_LAYOUT_ARRAY);
-        DIOPI_CHECKCNNL(cnnlNegTensor(handle, input_desc.get(), input_tensor.data(), out_desc.get(), out_tensor.data()));
-    }
+    std::vector<DiopiTensor*> pTensors{&input_tensor};
+    std::set<diopiDtype_t> supportedDtypes{diopi_dtype_float16, diopi_dtype_float32};
+    autoCastTensorType(ctx, pTensors, supportedDtypes);
+    DiopiTensor input_tensor_tmp = *pTensors[0];
+    DiopiTensor out_tensor_tmp = dataTypeCast(ctx, out_tensor, input_tensor_tmp.dtype());
+
+    CnnlTensorDesc input_desc(input_tensor_tmp, CNNL_LAYOUT_ARRAY);
+    CnnlTensorDesc out_desc(out_tensor_tmp, CNNL_LAYOUT_ARRAY);
+    DIOPI_CALLCNNL(cnnlNegTensor(handle, input_desc.get(), input_tensor_tmp.data(), out_desc.get(), out_tensor_tmp.data()));
+    dataTypeCast(ctx, out_tensor, out_tensor_tmp);
+    return diopiSuccess;
+}
+
+DIOPI_API diopiError_t diopiNegInp(diopiContextHandle_t ctx, diopiTensorHandle_t input) {
+    DIOPI_CALL(diopiNeg(ctx, input, input));
     return diopiSuccess;
 }
 
