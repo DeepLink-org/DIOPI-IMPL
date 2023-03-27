a618a8fe8642889df1090599103599465029603c
duanxiang <duanxiang@cambricon.com>
2023-03-27 10:58:02 +0000

    fix sgd

--- camb/functions/sgd.cpp
+++ camb/functions/sgd.cpp
@@ -7,23 +7,13 @@
 namespace impl {
 namespace camb {
 
-extern "C" DIOPI_API diopiError_t diopiSgd(diopiContextHandle_t ctx,
-                                           diopiTensorHandle_t w,
-                                           diopiTensorHandle_t dw,
-                                           diopiTensorHandle_t buf,
-                                           double lr,
-                                           double momentum,
-                                           double dampening,
-                                           double weight_decay,
-                                           bool nesterov) {
+extern "C" DIOPI_API diopiError_t diopiSgd(diopiContextHandle_t ctx, diopiTensorHandle_t w, diopiTensorHandle_t dw, diopiTensorHandle_t buf, double lr,
+                                           double momentum, double dampening, double weight_decay, bool nesterov) {
     cnnlHandle_t handle = cnnlHandlePool.get(ctx);
 
     auto w_tensor = DiopiTensor(w);
     auto dw_tensor = DiopiTensor(dw);
     auto buf_tensor = DiopiTensor(buf);
-
-    CnnlTensorDesc w_desc(w_tensor, CNNL_LAYOUT_ARRAY);
-    CnnlTensorDesc dw_desc(dw_tensor, CNNL_LAYOUT_ARRAY);
 
     // a = a * scale_a + b * scale_b;
     auto add_mul_func = [&](auto &a, float scale_a, auto b, float scale_b) {
@@ -40,7 +30,7 @@ extern "C" DIOPI_API diopiError_t diopiSgd(diopiContextHandle_t ctx,
         if (workspace_size != 0) {
             workspace = requiresBuffer(ctx, workspace_size).data();
         }
-
+        
         DIOPI_CALLCNNL(cnnlBiasAdd(handle, &scale_b, b_desc.get(), b.data(), workspace, workspace_size, &scale_a, a_desc.get(), a.data()));
         return diopiSuccess;
     };
@@ -54,11 +44,12 @@ extern "C" DIOPI_API diopiError_t diopiSgd(diopiContextHandle_t ctx,
                 DIOPI_CALL(add_mul_func(dw_tensor, 1.0, dw_tensor, momentum));
             }
         } else {
-            CnnlTensorDesc buf_desc(buf_tensor, CNNL_LAYOUT_ARRAY);
             DIOPI_CALL(add_mul_func(buf_tensor, momentum, dw_tensor, (1.0 - dampening)));
             if (nesterov) {
                 DIOPI_CALL(add_mul_func(dw_tensor, 1.0, buf_tensor, momentum));
             } else {
+                CnnlTensorDesc dw_desc(dw_tensor, CNNL_LAYOUT_ARRAY);
+                CnnlTensorDesc buf_desc(buf_tensor, CNNL_LAYOUT_ARRAY);
                 DIOPI_CALLCNNL(cnnlCopy(handle, buf_desc.get(), buf_tensor.data(), dw_desc.get(), dw_tensor.data()));
             }
         }
@@ -78,6 +69,8 @@ extern "C" DIOPI_API diopiError_t diopiSgd(diopiContextHandle_t ctx,
         DIOPI_CALLCNNL(cnnlSetTensorDescriptor(lr_half_desc.get(), CNNL_LAYOUT_ARRAY, CNNL_DTYPE_HALF, 1, shape.data()));
         cnnlCastDataType(handle, lr_desc.get(), lr_tensor.data(), CNNL_CAST_FLOAT_TO_HALF, lr_half_desc.get(), lr_tensor.data());
     }
+    CnnlTensorDesc w_desc(w_tensor, CNNL_LAYOUT_ARRAY);
+    CnnlTensorDesc dw_desc(dw_tensor, CNNL_LAYOUT_ARRAY);
     DIOPI_CALLCNNL(cnnlGradientDescent(handle, dw_desc.get(), dw_tensor.data(), lr_tensor.data(), w_desc.get(), w_tensor.data()));
     return diopiSuccess;
 }
