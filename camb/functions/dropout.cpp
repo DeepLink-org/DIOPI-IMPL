0469e821e25379fe8edfdd2f7817a22ee741f98e
zhangqiu <zhangqiu@sensetime.com>
2023-03-30 11:42:54 +0800

    fix lint, fix rand_seed, fix dtypecast

--- camb/functions/dropout.cpp
+++ camb/functions/dropout.cpp
@@ -17,14 +17,22 @@ diopiDropout(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiTensorHandl
         auto output_tensor = DiopiTensor(out);
         auto mask_tensor = DiopiTensor(mask);
 
+        // Do this Check to use DIOPI-TEST because non-float data not supported in PyTorch unless p==0
         DIOPI_CHECK(((DiopiDataType::isFloatPoint(input_tensor.dtype()) || p == 0)), "result type Float can't be cast to the desired type");
-        std::vector<DiopiTensor*> pTensors{&input_tensor, &output_tensor};
+        std::vector<DiopiTensor*> pTensors{&input_tensor};
         std::set<diopiDtype_t> supportedDtypes{
             diopi_dtype_int8, diopi_dtype_uint8, diopi_dtype_int16, diopi_dtype_int32, diopi_dtype_float16, diopi_dtype_float32};
         autoCastTensorType(ctx, pTensors, supportedDtypes);
 
+        DiopiTensor output_tensor_temp;
+        if ((output_tensor.dtype() != input_tensor.dtype())) {
+            output_tensor_temp = dataTypeCast(ctx, output_tensor, input_tensor.dtype());
+        } else {
+            output_tensor_temp = DiopiTensor(out);
+        }
+
         CnnlTensorDesc input_desc(input_tensor, CNNL_LAYOUT_ARRAY);
-        CnnlTensorDesc output_desc(output_tensor, CNNL_LAYOUT_ARRAY);
+        CnnlTensorDesc output_desc(output_tensor_temp, CNNL_LAYOUT_ARRAY);
         CnnlTensorDesc mask_desc(mask_tensor, CNNL_LAYOUT_ARRAY);
 
         // create and set the rand_generator
@@ -45,18 +53,28 @@ diopiDropout(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiTensorHandl
         void* kernel_params = nullptr;
         kernel_params = requiresBuffer(ctx, size_kernel).data();
         cnnlRandMakeMTGP32Constants(handle, params, kernel_params);
-        int rand_seed = rand();
+        int rand_seed = time(NULL);
         cnnlRandMakeMTGP32KernelState(handle, state, params, kernel_params, rand_seed);
-
-        DIOPI_CALLCNNL(cnnlFusedDropout_v2(
-            handle, generator, input_desc.get(), input_tensor.data(), p, state, mask_desc.get(), mask_tensor.data(), output_desc.get(), output_tensor.data()));
+        float p_ = float(p);
+        DIOPI_CALLCNNL(cnnlFusedDropout_v2(handle,
+                                           generator,
+                                           input_desc.get(),
+                                           input_tensor.data(),
+                                           p_,
+                                           state,
+                                           mask_desc.get(),
+                                           mask_tensor.data(),
+                                           output_desc.get(),
+                                           output_tensor_temp.data()));
         DIOPI_CALLCNNL(cnnlRandDestroyGenerator(generator));
+
+        if (output_tensor_temp.dtype() != output_tensor.dtype()) {
+            dataTypeCast(ctx, output_tensor, output_tensor_temp);
+        }
+
         return diopiSuccess;
     } else {
-        diopiScalar_t scale;
-        scale.stype = diopi_dtype_float64;
-        scale.fval = 1;
-        diopiMulScalar(ctx, out, input, &scale);
+        diopiCopyInp(ctx, input, out);
         return diopiSuccess;
     }
 }
