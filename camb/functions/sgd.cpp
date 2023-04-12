e95bd28b42770706decbd441f0f79df520641862
yangbofun <yangbofun@163.com>
2023-04-12 17:30:45 +0800

    fix sgd

--- camb/functions/sgd.cpp
+++ camb/functions/sgd.cpp
@@ -15,28 +15,29 @@
 namespace impl {
 namespace camb {
 
-extern "C" DIOPI_API diopiError_t diopiSgd(diopiContextHandle_t ctx,
-                                           diopiTensorHandle_t w,
-                                           diopiTensorHandle_t dw,
-                                           diopiTensorHandle_t buf,
-                                           double lr,
-                                           double momentum,
-                                           double dampening,
-                                           double weight_decay,
-                                           bool nesterov) {
+extern "C" DIOPI_API diopiError_t diopiSgd(diopiContextHandle_t ctx, diopiTensorHandle_t w, diopiTensorHandle_t dw, diopiTensorHandle_t buf, double lr,
+                                           double momentum, double dampening, double weight_decay, bool nesterov) {
     cnnlHandle_t handle = cnnlHandlePool.get(ctx);
 
     DiopiTensor w_tensor(w);
     DiopiTensor dw_tensor(dw);
-    DiopiTensor buf_tensor(buf);
-
-    std::vector<DiopiTensor *> pTensors{&dw_tensor, &buf_tensor};
+    DiopiTensor buf_tensor;
+    DiopiTensor buf_tensor_tmp;
+    std::vector<DiopiTensor *> pTensors;
+    if (buf != nullptr) {
+        buf_tensor = DiopiTensor(buf);
+        buf_tensor_tmp = buf_tensor;
+        pTensors = std::vector<DiopiTensor *>{&dw_tensor, &buf_tensor_tmp};
+    } else {
+        pTensors = std::vector<DiopiTensor *>{&dw_tensor};
+    }
     std::set<diopiDtype_t> supportedDtypes{diopi_dtype_float16, diopi_dtype_float32};
     DIOPI_CALL(autoCastTensorType(ctx, pTensors, supportedDtypes));
 
     DiopiTensor w_tensor_tmp = w_tensor;
     if (dw_tensor.dtype() != w_tensor_tmp.dtype()) {
         w_tensor_tmp = requiresTensor(ctx, w_tensor.shape(), dw_tensor.dtype());
+        DIOPI_CALL(dataTypeCast(ctx, w_tensor_tmp, w_tensor));
     }
 
     CnnlTensorDesc w_desc_tmp(w_tensor_tmp, CNNL_LAYOUT_ARRAY);
@@ -67,17 +68,14 @@ extern "C" DIOPI_API diopiError_t diopiSgd(diopiContextHandle_t ctx,
     }
     if (momentum != 0) {
         if (buf == nullptr) {
-            if (nesterov) {
-                DIOPI_CALL(add_mul_func(dw_tensor, 1.0, dw_tensor, momentum));
-            }
+            buf_tensor_tmp = dw_tensor;
         } else {
-            CnnlTensorDesc buf_desc(buf_tensor, CNNL_LAYOUT_ARRAY);
-            DIOPI_CALL(add_mul_func(buf_tensor, momentum, dw_tensor, (1.0 - dampening)));
-            if (nesterov) {
-                DIOPI_CALL(add_mul_func(dw_tensor, 1.0, buf_tensor, momentum));
-            } else {
-                DIOPI_CALLCNNL(cnnlCopy(handle, buf_desc.get(), buf_tensor.data(), dw_desc.get(), dw_tensor.data()));
-            }
+            DIOPI_CALL(add_mul_func(buf_tensor_tmp, momentum, dw_tensor, (1.0 - dampening)));
+        }
+        if (nesterov) {
+            DIOPI_CALL(add_mul_func(dw_tensor, 1.0, buf_tensor_tmp, momentum));
+        } else {
+            dw_tensor = buf_tensor_tmp;
         }
     }
 
@@ -85,10 +83,13 @@ extern "C" DIOPI_API diopiError_t diopiSgd(diopiContextHandle_t ctx,
     diopiSize_t size(shape.data(), shape.size());
     DiopiTensor lr_tensor;
     diopiScalar_t lr_scalar{diopi_dtype_float64, {lr}};
-    makeTensorFromScalar(ctx, &lr_scalar, lr_tensor);
-    dataTypeCast(ctx, lr_tensor, dw_tensor.dtype());
+    DIOPI_CALL(makeTensorFromScalar(ctx, &lr_scalar, lr_tensor));
+    DIOPI_CALL(dataTypeCast(ctx, lr_tensor, dw_tensor.dtype()));
     DIOPI_CALLCNNL(cnnlGradientDescent(handle, dw_desc.get(), dw_tensor.data(), lr_tensor.data(), w_desc_tmp.get(), w_tensor_tmp.data()));
     DIOPI_CALL(dataTypeCast(ctx, w_tensor, w_tensor_tmp));
+    if (buf != nullptr) {
+        DIOPI_CALL(dataTypeCast(ctx, buf_tensor, buf_tensor_tmp));
+    }
     return diopiSuccess;
 }
 
