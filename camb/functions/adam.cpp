a7499c52ced6bf01c3b494ab0a79674254a7959f
duanxiang <duanxiang@cambricon.com>
2023-04-18 06:59:02 +0000

    add adam

--- camb/functions/adam.cpp
+++ camb/functions/adam.cpp
@@ -4,8 +4,6 @@
 #include "../diopi_helper.hpp"
 #include "adam.hpp"
 
-#include "dbg.h"
-
 namespace impl {
 namespace camb {
 
@@ -13,7 +11,6 @@ extern "C" diopiError_t diopiAdam(diopiContextHandle_t ctx, diopiTensorHandle_t 
                                   diopiTensorHandle_t exp_avg_sq, diopiTensorHandle_t max_exp_avg_sq, float lr, float beta1, float beta2, float eps,
                                   float weight_decay, int64_t step, bool amsgrad) {
     cnrtQueue_t queue = getStream(ctx);
-
     DiopiTensor input_tensor = DiopiTensor(input);
     DiopiTensor grad_tensor = DiopiTensor(grad);
     DiopiTensor exp_avg_tensor = DiopiTensor(exp_avg);
@@ -29,27 +26,14 @@ extern "C" diopiError_t diopiAdam(diopiContextHandle_t ctx, diopiTensorHandle_t 
     std::vector<DiopiTensor *> tensors{&input_casted, &grad_casted, &exp_avg_casted, &exp_avg_sq_casted, &max_exp_avg_sq_casted};
     DIOPI_CALL(autoCastTensorType(ctx, tensors, {diopi_dtype_float16, diopi_dtype_float32}));
 
-    AddressList g, m, v, v_max, p;
-    g.addresses[0] = grad_casted.data();
-    m.addresses[0] = exp_avg_casted.data();
-    v.addresses[0] = exp_avg_sq_casted.data();
-    v_max.addresses[0] = max_exp_avg_sq_casted.data();
-    p.addresses[0] = input_casted.data();
-
-    SizeList sizes;
-    sizes.sizes[0] = input_casted.numel();
-
-    float bias_correction = 1;
     float beta1_correction_recip = 1;
     float beta2_correction_recip = 1;
-    if (bias_correction == 1) {
-        beta1_correction_recip = 1 / (1 - std::pow(beta1, step));
-        beta2_correction_recip = 1 / (1 - std::pow(beta2, step));
-    }
+    beta1_correction_recip = 1 / (1 - std::pow(beta1, step));
+    beta2_correction_recip = 1 / (1 - std::pow(beta2, step));
     float epsilon_correction = eps / std::sqrt(beta2_correction_recip);
     float learning_rate_correction = lr * beta1_correction_recip / std::sqrt(beta2_correction_recip);
     
-    int adam_mode = 0;
+    int adamw_mode = 0;
     float decay_correction = 1 - lr * weight_decay ; 
     cnrtDim3_t k_dim;
     int cluster_count = 0;
@@ -68,18 +52,18 @@ extern "C" diopiError_t diopiAdam(diopiContextHandle_t ctx, diopiTensorHandle_t 
     }
     
     bang_fused_adam_internal(
-        g,
-        m,
-        v,
-        v_max,
-        p,
-        sizes,
+        grad_casted.data(),
+        exp_avg_casted.data(),
+        exp_avg_sq_casted.data(),
+        max_exp_avg_sq_casted.data(),
+        input_casted.data(),
+        input_casted.numel(),
         1,
         beta1,
         beta2,
         epsilon_correction,
         learning_rate_correction,
-        adam_mode,
+        adamw_mode,
         weight_decay,
         decay_correction,
         k_dim,
@@ -88,6 +72,11 @@ extern "C" diopiError_t diopiAdam(diopiContextHandle_t ctx, diopiTensorHandle_t 
         cnrt_type,
         amsgrad
     );
+    DIOPI_CALL(dataTypeCast(ctx, grad_tensor, grad_casted));
+    DIOPI_CALL(dataTypeCast(ctx, input_tensor, input_casted));
+    DIOPI_CALL(dataTypeCast(ctx, exp_avg_tensor, exp_avg_casted));
+    DIOPI_CALL(dataTypeCast(ctx, exp_avg_sq_tensor, exp_avg_sq_casted));
+    DIOPI_CALL(dataTypeCast(ctx, max_exp_avg_sq_tensor, max_exp_avg_sq_casted));
     return diopiSuccess;
 }
 }  // namespace camb
--- camb/functions/adam.hpp
+++ camb/functions/adam.hpp
@@ -1,22 +1,22 @@
-#define MAX_TENSOR_NUM 31
+// #define MAX_TENSOR_NUM 31
 
-typedef void* MLUaddr;
+// typedef void* MLUaddr;
 
-struct AddressList {
-    void* addresses[MAX_TENSOR_NUM];
-};
+// struct AddressList {
+//     void* addresses[MAX_TENSOR_NUM];
+// };
 
-struct SizeList {
-    int sizes[MAX_TENSOR_NUM];
-};
+// struct SizeList {
+//     int sizes[MAX_TENSOR_NUM];
+// };
 
 void bang_fused_adam_internal(
-    AddressList grad, 
-    AddressList m, 
-    AddressList v,
-    AddressList v_max,
-    AddressList variable, 
-    SizeList sizes, 
+    void* grad, 
+    void* m, 
+    void* v,
+    void* v_max,
+    void* variable, 
+    size_t sizes, 
     int tensor_num, 
     float beta1, 
     float beta2,
