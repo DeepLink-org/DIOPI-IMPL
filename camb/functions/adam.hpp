17115fe7650ded25be95cb48ab5ecd8355d47ecf
duanxiang <duanxiang@cambricon.com>
2023-04-18 10:56:29 +0000

    format

--- camb/functions/adam.hpp
+++ camb/functions/adam.hpp
@@ -1,23 +1,6 @@
 #include <cnrt.h>
 #include <stdint.h>
 
-void bang_fused_adam_internal(
-    void* grad, 
-    void* m, 
-    void* v,
-    void* v_max,
-    void* variable, 
-    size_t sizes, 
-    int tensor_num, 
-    float beta1, 
-    float beta2,
-    float epsilon_correction, 
-    float learning_rate_correction, 
-    int adam_mode, 
-    float decay, 
-    float decay_correction, 
-    cnrtDim3_t k_dim,
-    cnrtFunctionType_t k_type, 
-    cnrtQueue_t queue, 
-    cnrtDataType_t cnrt_type,
-    bool amsgrad);
+void bang_fused_adam_internal(void* grad, void* m, void* v, void* v_max, void* variable, size_t sizes, int tensor_num, float beta1, float beta2,
+                              float epsilon_correction, float learning_rate_correction, int adam_mode, float decay, float decay_correction, cnrtDim3_t k_dim,
+                              cnrtFunctionType_t k_type, cnrtQueue_t queue, cnrtDataType_t cnrt_type, bool amsgrad);
