937ec1d866f99740f83216117c51fce7363def32
duanxiang <duanxiang@cambricon.com>
2023-04-18 02:53:13 +0000

    tmp save

--- camb/functions/adam.hpp
+++ camb/functions/adam.hpp
@@ -10,6 +10,23 @@ struct SizeList {
     int sizes[MAX_TENSOR_NUM];
 };
 
-void bang_fused_adam_internal(AddressList grad, AddressList m, AddressList v, AddressList variable, SizeList sizes, int tensor_num, float beta1, float beta2,
-                              float epsilon_correction, float learning_rate_correction, int adam_mode, float decay, float decay_correction, cnrtDim3_t k_dim,
-                              cnrtFunctionType_t k_type, cnrtQueue_t queue, cnrtDataType_t cnrt_type);
+void bang_fused_adam_internal(
+    AddressList grad, 
+    AddressList m, 
+    AddressList v,
+    AddressList v_max,
+    AddressList variable, 
+    SizeList sizes, 
+    int tensor_num, 
+    float beta1, 
+    float beta2,
+    float epsilon_correction, 
+    float learning_rate_correction, 
+    int adam_mode, 
+    float decay, 
+    float decay_correction, 
+    cnrtDim3_t k_dim,
+    cnrtFunctionType_t k_type, 
+    cnrtQueue_t queue, 
+    cnrtDataType_t cnrt_type,
+    bool amsgrad);
