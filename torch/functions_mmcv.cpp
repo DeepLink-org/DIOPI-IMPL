27076a047412327293643ae4a5110be2f5df0db7
dongkaixing <408244909@qq.com>
2023-05-18 10:48:58 +0800

    Add diopi impl for mmcv ext op nms, voxelization, sigmoid_focal_loss

--- torch/functions_mmcv.cpp
+++ torch/functions_mmcv.cpp
@@ -29,6 +29,7 @@ diopiError_t diopiRoiAlignMmcv(diopiContextHandle_t ctx, diopiTensorHandle_t out
                                diopiTensorHandle_t argmax_x_, diopiConstTensorHandle_t input_, diopiConstTensorHandle_t rois_,
                                int64_t aligned_height, int64_t aligned_width, int64_t sampling_ratio, int64_t pool_mode,
                                float spatial_scale, bool aligned) {
+  std::cout << "Into diopi_real_impl diopiRoiAlignMmcv\n";
   auto input = impl::aten::buildATen(input_);
   auto rois = impl::aten::buildATen(rois_);
   auto output = impl::aten::buildATen(output_);
@@ -53,4 +54,98 @@ diopiError_t diopiRoiAlignBackwardMmcv(diopiContextHandle_t ctx, diopiTensorHand
       aligned_width, spatial_scale, sampling_ratio, pool_mode, aligned);
 }
 
+diopiError_t diopiSigmoidFocalLossMmcv(diopiContextHandle_t ctx,
+                                       diopiTensorHandle_t output_,
+                                       diopiConstTensorHandle_t input_,
+                                       diopiConstTensorHandle_t target_,
+                                       diopiConstTensorHandle_t weight_,
+                                       float gamma,
+                                       float alpha) {
+  auto output = impl::aten::buildATen(output_);
+  auto input = impl::aten::buildATen(input_);
+  auto target = impl::aten::buildATen(target_);
+  auto weight = impl::aten::buildATen(weight_);
+  mmcv::ops::SigmoidFocalLossForwardCUDAKernelLauncher(input, target, weight, output,
+                                                       gamma, alpha);
+}
+
+diopiError_t diopiSigmoidFocalLossBackwardMmcv(diopiContextHandle_t ctx,
+                                               diopiTensorHandle_t grad_input_,
+                                               diopiConstTensorHandle_t input_,
+                                               diopiConstTensorHandle_t target_,
+                                               diopiConstTensorHandle_t weight_,
+                                               float gamma,
+                                               float alpha) {
+  auto grad_input = impl::aten::buildATen(grad_input_);
+  auto input = impl::aten::buildATen(input_);
+  auto target = impl::aten::buildATen(target_);
+  auto weight = impl::aten::buildATen(weight_);
+  mmcv::ops::SigmoidFocalLossBackwardCUDAKernelLauncher(input, target, weight, grad_input,
+                                                        gamma, alpha);
+}
+
+diopiError_t diopiHardVoxelizeMmcv(diopiContextHandle_t ctx,
+                                             diopiTensorHandle_t voxels_,
+                                             diopiTensorHandle_t coors_,
+                                             diopiTensorHandle_t num_points_per_voxel_,
+                                             diopiTensorHandle_t voxel_num_,
+                                             diopiConstTensorHandle_t points_,
+                                             diopiConstTensorHandle_t voxel_size_,
+                                             diopiConstTensorHandle_t coors_range_,
+                                             const int64_t max_points,
+                                             const int64_t max_voxels,
+                                             const int64_t NDim,
+                                             const bool deterministic) {
+  auto voxels = impl::aten::buildATen(voxels_);
+  auto coors = impl::aten::buildATen(coors_);
+  auto num_points_per_voxel = impl::aten::buildATen(num_points_per_voxel_);
+  auto voxel_num = impl::aten::buildATen(voxel_num_);
+  auto points = impl::aten::buildATen(points_);
+  auto voxel_size = impl::aten::buildATen(voxel_size_);
+  auto coors_range = impl::aten::buildATen(coors_range_);
+
+  int64_t *voxel_num_data = voxel_num.data_ptr<int64_t>();
+  std::vector<float> voxel_size_v(
+      voxel_size.data_ptr<float>(),
+      voxel_size.data_ptr<float>() + voxel_size.numel());
+  std::vector<float> coors_range_v(
+      coors_range.data_ptr<float>(),
+      coors_range.data_ptr<float>() + coors_range.numel());
+
+  if (deterministic) {
+    *voxel_num_data = mmcv::ops::HardVoxelizeForwardCUDAKernelLauncher(
+      points, voxels, coors, num_points_per_voxel, voxel_size_v, coors_range_v,
+      max_points, max_voxels, NDim);
+  } else {
+    *voxel_num_data = mmcv::ops::NondeterministicHardVoxelizeForwardCUDAKernelLauncher(
+        points, voxels, coors, num_points_per_voxel, voxel_size_v,
+        coors_range_v, max_points, max_voxels, NDim);
+  }
+}
+
+/**
+ * @brief Convert kitti points(N, >=3) to voxels(max_points == -1 or max_voxels
+ * == -1).
+ *  @param[in] ctx diopi context.
+ * @sa definition of other parameters, refer to diopiHardVoxelize().
+ */
+diopiError_t diopiDynamicVoxelizeMmcv(diopiContextHandle_t ctx,
+                                                diopiTensorHandle_t coors_,
+                                                diopiConstTensorHandle_t points_,
+                                                diopiConstTensorHandle_t voxel_size_,
+                                                diopiConstTensorHandle_t coors_range_,
+                                                const int64_t NDim) {
+  auto coors = impl::aten::buildATen(coors_);
+  auto points = impl::aten::buildATen(points_);
+  auto voxel_size = impl::aten::buildATen(voxel_size_);
+  auto coors_range = impl::aten::buildATen(coors_range_);
+
+  std::vector<float> voxel_size_v(
+      voxel_size.data_ptr<float>(),
+      voxel_size.data_ptr<float>() + voxel_size.numel());
+  std::vector<float> coors_range_v(
+      coors_range.data_ptr<float>(),
+      coors_range.data_ptr<float>() + coors_range.numel());
+  mmcv::ops::DynamicVoxelizeForwardCUDAKernelLauncher(points, coors, voxel_size_v, coors_range_v, NDim);
+}
 }  // extern "C"
